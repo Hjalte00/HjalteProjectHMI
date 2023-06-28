44047537e9ec7ab88befcde26e5e3edbf3a9ef34
Hjalte97 <Hjalte.Hellenberg@rockwellautomation.com>
2023-06-28 08:32:10 +0200

    Working visibility!

---
+++ ProjectFiles/NetSolution/RuntimeNetLogic1.cs
@@ -0,0 +1,33 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.HMIProject;
+using FTOptix.NativeUI;
+using FTOptix.UI;
+using FTOptix.EventLogger;
+using FTOptix.Alarm;
+using FTOptix.Recipe;
+using FTOptix.DataLogger;
+using FTOptix.SQLiteStore;
+using FTOptix.Store;
+using FTOptix.RAEtherNetIP;
+using FTOptix.Retentivity;
+using FTOptix.CoreBase;
+using FTOptix.CommunicationDriver;
+using FTOptix.NetLogic;
+using FTOptix.Core;
+#endregion
+
+public class RuntimeNetLogic1 : BaseNetLogic
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+}
